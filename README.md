dc70720c2c809ee13ac812ecb5195b061684d29c
Richard Van Nairn <rvannairn@googlemail.com>
2022-09-10 09:49:39 +0100

    Change refactored traingle function and added to README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Triangle Test
+
+I wrote a function that takes three values (a,b,c) and returns a string detailing what type of triangle it is Equilateral, Isosceles or Scalene.
+
+Should you try and pass a string instead of an integer, or a integer less than 1 the function will return a string saying Invalid.
+
+## Run Tests
+
+- Confirm python is installed on your machine by running `python --version` in your terminal
+
+* If its not installed go to [Python](https://www.python.org/downloads/) to download it.
+
+* With python installed run `python test_triangle.py` in the terminal to run the tests found in `test_triangle.py`.
--- triangle.py
+++ triangle.py
@@ -1,13 +1,16 @@
 
 def triangle (a,b,c):
+    equilateral = a > 0 and b > 0 and c > 0 and a == b and a == c and b == c and b == a and c == a and c == a
+    isosceles = a > 0 and b > 0 and c > 0 and a == b and b == a and a != c and b != c or a == c and c == a and b != a and b != c or b == c and c == b and b != a and c != a
+    scalene = a > 0 and b > 0 and c > 0 and a != b and a != c and b != c and b != a and c != a and c != a
     if isinstance(a,str) or isinstance(b,str) or isinstance(c,str):
          return "Invalid"   
     else:
-        if a > 0 and b > 0 and c > 0 and a == b and a == c and b == c and b == a and c == a and c == a:
+        if equilateral == True:
             return "Equilateral"
-        elif a > 0 and b > 0 and c > 0 and a == b and b == a and a != c and b != c or a == c and c == a and b != a and b != c or b == c and c == b and b != a and c != a:
+        elif isosceles == True:
             return "Isosceles"
-        elif a > 0 and b > 0 and c > 0 and a != b and a != c and b != c and b != a and c != a and c != a:
+        elif scalene == True:
             return "Scalene"
         else:
             return "Invalid"
