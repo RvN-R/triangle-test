6908cbebc32f433683c84110d50f4aeb1bd75d77
Richard Van Nairn <rvannairn@googlemail.com>
2022-09-10 13:41:55 +0100

    Change additional refactor of triangle function

--- triangle.py
+++ triangle.py
@@ -1,8 +1,8 @@
 
 def triangle (a,b,c):
-    equilateral = a > 0 and b > 0 and c > 0 and a == b and a == c and b == c and b == a and c == a and c == a
+    equilateral = a > 0 and b > 0 and c > 0 and a == b and a == c
     isosceles = a > 0 and b > 0 and c > 0 and a == b and b == a and a != c and b != c or a == c and c == a and b != a and b != c or b == c and c == b and b != a and c != a
-    scalene = a > 0 and b > 0 and c > 0 and a != b and a != c and b != c and b != a and c != a and c != a
+    scalene = a > 0 and b > 0 and c > 0 and a != b and a != c
     if isinstance(a,str) or isinstance(b,str) or isinstance(c,str):
          return "Invalid"   
     else:
